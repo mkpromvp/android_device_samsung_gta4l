fdac2857eb747bbbc6db6cf50702da3f6d197118
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-04-30 03:41:35 +0300

    Update copyright and product identifiers in mk file

--- lineage_gta4l.mk
+++ lineage_gta4l.mk
@@ -1,17 +1,8 @@
 #
-# Copyright (C) 2020-2023 The LineageOS Project
+# Copyright (C) 2020-2025 The LineageOS Project
+# Copyright (C) 2025 Project Infinity-X
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Licensed under the Apache License, Version 2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -21,19 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gta4l device
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
+# Inherit some common Infinity-X stuff (بدل Lineage)
+$(call inherit-product, vendor/infinity/config/common_full_tablet.mk)
 
-# Device identifier. This must come after all inclusions
+# Device identifier
 PRODUCT_DEVICE := gta4l
-PRODUCT_NAME := lineage_gta4l
+PRODUCT_NAME := infinity_gta4l
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T505
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
-# Use the latest approved GMS identifiers
+# Build prop overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="gta4lxx-user 12 SP1A.210812.016 T505XXS8CXG1 release-keys" \
     BuildFingerprint=samsung/gta4leea/gta4l:12/SP1A.210812.016/T505XXS8CXG1:user/release-keys \
