ff0a5d623a2801b5a3a0d762d615ddd455755e8f
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-20 12:12:53 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-# InfinityX Build Setup for Samsung Tab A7 (gta4l)
+# matrixx Build Setup for Samsung Tab A7 (gta4l)
 for variant in user userdebug eng; do
     add_lunch_combo matrixx_gta4l-$variant
 done
