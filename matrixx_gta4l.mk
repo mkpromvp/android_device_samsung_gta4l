29574f5c17aa38ede667f8b34e1b3d57174e3335
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-20 11:57:36 +0300

    Create matrixx_gta4l.mk

---
+++ matrixx_gta4l.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2020-2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from gta4l device
+$(call inherit-product, device/samsung/gta4l/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/matrixx/config/common_full_tablet.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gta4l
+PRODUCT_NAME := matrixx_gta4l
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T505
+PRODUCT_MANUFACTURER := samsung
+
+# Matrexx Flags
+MATRIXX_MAINTAINER := MkProMVP
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="gta4lxx-user 12 SP1A.210812.016 T505XXS8CXG1 release-keys" \
+    BuildFingerprint=samsung/gta4leea/gta4l:12/SP1A.210812.016/T505XXS8CXG1:user/release-keys \
+    DeviceProduct=gta4lxx \
+    SystemName=gta4lxx
