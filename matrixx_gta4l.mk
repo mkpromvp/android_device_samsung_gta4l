dbfaf8655fb1c064f37c7133394ab4286dde3fb2
mkpromvp <tabmohamedpro@gmail.com>
2026-06-20 09:20:05 +0300

    feat: Add matrixx_gta4l.mk and update AndroidProducts.mk

--- matrixx_gta4l.mk
+++ matrixx_gta4l.mk
@@ -1,21 +1,8 @@
-#
-# Copyright (C) 2020-2025 The LineageOS Project
-# Copyright (C) 2025 Project Infinity-X
-# Copyright (C) 2025 Project matrixx
-# Licensed under the Apache License, Version 2.0
-#
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from gta4l device
 $(call inherit-product, device/samsung/gta4l/device.mk)
-
-# Inherit some common matrixx (بدل Lineage)
 $(call inherit-product, vendor/matrixx/config/common_full_tablet.mk)
 
-# Device identifier
 PRODUCT_DEVICE := gta4l
 PRODUCT_NAME := matrixx_gta4l
 PRODUCT_BRAND := samsung
@@ -24,12 +11,10 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
-# Build prop overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="gta4lxx-user 12 SP1A.210812.016 T505XXS8CXG1 release-keys" \
     BuildFingerprint=samsung/gta4leea/gta4l:12/SP1A.210812.016/T505XXS8CXG1:user/release-keys \
     DeviceProduct=gta4lxx \
     SystemName=gta4lxx
 
-# Maintainer flag
 MATRIXX_MAINTAINER := MkProMVP
